b47d9d5284964b908a8947d0e95ec4dca1449d16
OrichalcosShunnoros <c3146092091@gmail.com>
2025-05-10 16:15:56 -0500

    act

--- app.ts
+++ app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import studentRoutes from './routes/student.routes';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
-app.use(studentRoutes)
+app.use(studentRoutes);
 
 const port = process.env.PORT || 3000;
 
--- repositories/studentRepository.ts
+++ repositories/studentRepository.ts
@@ -3,18 +3,18 @@ import { studentDto } from '../dto/studentDto';
 
 export class StudentRepository {
     getAllStudents = async () => {
-        const [rows] = await db.query('SELECT * FROM students');
+        const [rows] = await db.query('SELECT * FROM student');
         return rows;
     }
 
     getStudentById = async (id: number) => {
-        const [rows]:any = await db.query('SELECT * FROM students WHERE id = ?', [id]);
+        const [rows]:any = await db.query('SELECT * FROM student WHERE id = ?', [id]);
         return rows[0];
     }
 
     createStudent = async (student: studentDto) => {
         const {full_name,id_type,id_number,birth_date,birth_country,birth_state, birth_city,phone_number,registration_year,is_active,grade_id, rank_id,residence_country, residence_state,residence_city,residence_neighborhood,residence_address,observations} = student;
-        const [result]: any = await db.query('INSERT INTO students (full_name,id_type,id_number,birth_date,birth_country,birth_state, birth_city,phone_number,registration_year,is_active,grade_id, rank_id,residence_country, residence_state,residence_city,residence_neighborhood,residence_address,observations) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [full_name,id_type,id_number,birth_date,birth_country,birth_state, birth_city,phone_number,registration_year,is_active,grade_id, rank_id,residence_country, residence_state,residence_city,residence_neighborhood,residence_address,observations]);
+        const [result]: any = await db.query('INSERT INTO student (full_name,id_type,id_number,birth_date,birth_country,birth_state, birth_city,phone_number,registration_year,is_active,grade_id, rank_id,residence_country, residence_state,residence_city,residence_neighborhood,residence_address,observations) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [full_name,id_type,id_number,birth_date,birth_country,birth_state, birth_city,phone_number,registration_year,is_active,grade_id, rank_id,residence_country, residence_state,residence_city,residence_neighborhood,residence_address,observations]);
         return result.insertId;
     }
 }
